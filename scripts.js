733382ec92caac71e6388b2ed49d0e92224028af
clentebanks <clentebanks0@gmail.com>
2022-10-16 13:38:28 -0600

    grrr

---
+++ scripts.js
@@ -0,0 +1,4 @@
+      /*ESTE SCRIPT PARA HACER FUNCIONAR EL TOOLTIP DE BOOTSTRAP 5*/
+      const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+      const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+       /*ESTE SCRIPT PARA HACER FUNCIONAR EL TOOLTIP DE BOOTSTRAP 5*/
